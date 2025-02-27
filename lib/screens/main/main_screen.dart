6c3720852bb1a142f5756adbddbb1269dc7afc85
jay_wu <jaywu3272@gmail.com>
2025-02-27 10:25:06 +0000

    Layout布局

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: Row(children: [
+      Expanded(
+        // default flex = 1
+        // and it takes 1/6 part of the screen
+        child: Container(
+          color: Colors.white,
+        ), // Container
+      ), //Expanded
+      Expanded(
+        //
+        flex: 5,
+        child: Container(
+          color: Colors.blue,
+        ), // Container
+      ), //Expanded
+    ])));
   }
 }
